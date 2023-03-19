a6d7613203aeb8f972c5a751d152b45dbead9fbc
EYTZIP <eyaltzipor5@gmail.com>
2023-03-19 15:36:30 +0200

    ParallelPrinting

--- ParallelPrinting/Program.cs
+++ ParallelPrinting/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//Solution.Assignment1Part1();
+//Solution.Assignment1Part2();
+//Solution.Assignment2("eyal");
+//Solution.Assignment2("BBC");
+//Solution.Assignment3Part1("sss");
+//Solution.Assignment3Part2("sss");
+//Solution.Assignment4Part1();
+Solution.Assignment4Part2();
+
+Console.ReadLine();
--- ParallelPrinting/Solution.cs
+++ ParallelPrinting/Solution.cs
@@ -1,53 +1,124 @@
 ﻿using System;
+using System.Net.WebSockets;
 
 public class Solution
-{
+{       
+    delegate void Print(string s,int num);
+    delegate void Print5(object s);
+
+
+    static Print printString = delegate (string s,int num)
+    {
+        for (int i = 0; i < num; i++)
+        {
+            Console.WriteLine(s + " , " + i);
+        }
+    };
+
+    static Print5 print5String = delegate (object s)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine((string)s + " , " + i);
+        }
+    };
+
+
+
+
     public static void Assignment1Part1()
     {
-        /*
-        * Write code here
-        */
+
+        Thread X = new Thread(() => printString("X",100));
+
+        Thread Y = new Thread(() => printString("Y",100));
+
+        Console.WriteLine("Y - " + Y.IsBackground);
+
+        Console.WriteLine("X - " + X.IsBackground);
+
+        X.Start();
+        Y.Start();
+
+
     }
 
     public static void Assignment1Part2()
     {
-        /*
-        * Write code here
-        */
+        Thread X = new Thread(() => printString("X", 100)) { IsBackground = true };
+
+
+        Thread Y = new Thread(() => printString("Y", 100)) { IsBackground = true };
+
+
+        Console.WriteLine("Y - " + Y.IsBackground);
+
+        Console.WriteLine("X - " + X.IsBackground);
+
+        X.Start();
+        Y.Start();
     }
 
     public static void Assignment2(string str)
     {
-        /*
-        * Write code here
-        */
+        Thread t1= new Thread(new ParameterizedThreadStart(print5String));
+        Thread t2= new Thread(new ParameterizedThreadStart(print5String));
+
+        t1.Start(str);
+        t2.Start(str);
+
     }
 
     public static void Assignment3Part1(string str)
     {
-        /*
-        * Write code here
-        */
+        ThreadPool.QueueUserWorkItem(new WaitCallback(print5String), str);
+        ThreadPool.QueueUserWorkItem(new WaitCallback(print5String), str);
+
     }
 
     public static void Assignment3Part2(string str)
     {
-        /*
-        * Write code here
-        */
+        int max = 25 * Environment.ProcessorCount;
+        ThreadPool.SetMaxThreads(max, max);
+
+        ThreadPool.QueueUserWorkItem(new WaitCallback(print5String), str);
+        ThreadPool.QueueUserWorkItem(new WaitCallback(print5String), str);
     }
 
     public static void Assignment4Part1()
     {
-        /*
-        * Write code here
-        */
+        Thread t1 = new Thread(() => { for (int i = 1; i <= 100; i++) Console.WriteLine(i); });
+        t1.Start();
+
+        t1.Join();
+        Thread.Sleep(2000);
+        Console.WriteLine("Thread ended printing");
     }
 
     public static void Assignment4Part2()
     {
-        /*
-        * Write code here
-        */
+        object lockKey = new object();
+        Thread t1 = new Thread(() => 
+        {
+            for (int i = 1; i <= 50 ; i++)
+                lock (lockKey)
+                {
+                    Console.WriteLine(i); 
+                }
+        });
+
+        Thread t2 = new Thread(() => 
+        {
+            for (int i = 51; i <= 100; i++)
+                lock (lockKey)
+                {
+                    Console.WriteLine(i);
+                }
+        });
+        t1.Start();
+        t2.Start();        
+        
+
+
     }
 }
