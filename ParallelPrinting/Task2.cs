e1a15dafb46793721a40331bc09c9c4f62c52f57
guyalfasi <guyalfasi@gmail.com>
2023-03-19 14:43:28 +0200

    finished parallel printing tasks

---
+++ ParallelPrinting/Task2.cs
@@ -0,0 +1,33 @@
+﻿public class Task2
+{
+    public Task2()
+    {
+    }
+    public void PrintSomething5Times(object obj)
+    {
+        string str = (string)obj;
+        Console.WriteLine($"{Thread.CurrentThread.Name} started");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} says {str}");
+            Thread.Sleep(100);
+        }
+        Console.WriteLine($"{Thread.CurrentThread.Name} ended");
+
+    }
+
+    public void RunAllThreads(string str)
+    {
+        Thread t1 = new Thread(new ParameterizedThreadStart(PrintSomething5Times));
+        Thread t2 = new Thread(new ParameterizedThreadStart(PrintSomething5Times));
+
+        t1.Name = "thread 1";
+        t2.Name = "thread 2";
+
+        t1.Start(str);
+        t2.Start(str);
+        t1.Join();
+        t2.Join();
+        Console.WriteLine("all threads ended");
+    }
+}
---
+++ ParallelPrinting/Task3.cs
@@ -0,0 +1,40 @@
+﻿public class Task3
+{
+    public Task3()
+    {
+
+    }
+    public void PrintSomething(object obj)
+    {
+        string something = (string)obj;
+        Console.WriteLine($"something {something}");
+        Thread.Sleep(new Random().Next(1000, 5000));
+        Console.WriteLine($"goodbye {something}");
+    }
+
+    public void RunAllThreadsByCPU(object obj)
+    {
+        int coreCount = Environment.ProcessorCount;
+        int MaxThreads = coreCount * 25;
+        ThreadPool.SetMaxThreads(MaxThreads, MaxThreads);
+        Console.WriteLine("max threads: {0}", MaxThreads);
+
+        for (int i = 1; i <= MaxThreads; i++)
+        {
+            ThreadPool.QueueUserWorkItem(PrintSomething, obj);
+        }
+        Thread.Sleep(5000);
+        Console.WriteLine("all threads finished");
+    }    
+    public void RunAllThreads(object obj, int corenum)
+    {
+        ThreadPool.SetMaxThreads(corenum, corenum);
+        Console.WriteLine("max threads: {0}", corenum);
+        for (int i = 1; i <= corenum; i++)
+        {
+            ThreadPool.QueueUserWorkItem(PrintSomething, obj);
+        }
+        Thread.Sleep(5000);
+        Console.WriteLine("all threads finished");
+    }
+}
