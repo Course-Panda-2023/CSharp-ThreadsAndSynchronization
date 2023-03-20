ec33e93542a0f4bb5ade2c345357575c3c3b10db
liavbatson <batsonliav@gmail.com>
2023-03-20 14:24:24 +0200

    Completed Parallel Printing

--- ParallelPrinting/Solution.cs
+++ ParallelPrinting/Solution.cs
@@ -1,53 +1,87 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 
 public class Solution
 {
+    static object locker;
+    public static void PrintX()
+    {
+        for (int i = 0; i < 100; i++)
+            Console.WriteLine("X");
+    }
+    public static void PrintY()
+    {
+        for (int i = 0; i < 100; i++)
+            Console.WriteLine("Y");
+    }
+    public static void PrintString(object s)
+    {
+        for (int i = 0; i < 5; i++)
+            Console.WriteLine(s);
+    }
+    public static void ThreadFunction(object s)
+    {
+        Console.WriteLine(s);
+    }
     public static void Assignment1Part1()
     {
-        /*
-        * Write code here
-        */
+        Thread t = new Thread(new ThreadStart(PrintX));
+        t.Start();
+        PrintY();
     }
-
     public static void Assignment1Part2()
     {
-        /*
-        * Write code here
-        */
+        Thread t = new Thread(new ThreadStart(PrintX)) { IsBackground = true };
+        t.Start();
+        PrintY();
     }
-
     public static void Assignment2(string str)
     {
-        /*
-        * Write code here
-        */
+        Thread t = new Thread(new ParameterizedThreadStart(PrintString));
+        t.Start(str);
+        PrintString(str);
     }
-
     public static void Assignment3Part1(string str)
     {
-        /*
-        * Write code here
-        */
+        ThreadPool.QueueUserWorkItem(ThreadFunction, str);
+        ThreadPool.QueueUserWorkItem(ThreadFunction, str);
+        Thread.Sleep(1000);
     }
-
-    public static void Assignment3Part2(string str)
+    public static void Assignment3Part2()
     {
-        /*
-        * Write code here
-        */
+        Console.WriteLine($"succeeded: {ThreadPool.SetMaxThreads(Environment.ProcessorCount * 25, Environment.ProcessorCount * 25)}");
     }
-
+    public static void PrintNumbersOneToHundred()
+    {
+        for (int i = 1; i <= 100; i++)
+            Console.WriteLine(i);
+    }
+    public static void PrintNumbersOneToHundredWithLock()
+    {
+        lock (locker)
+        {
+            PrintNumbersOneToHundred();
+        }
+    }
     public static void Assignment4Part1()
     {
-        /*
-        * Write code here
-        */
+        Thread t = new Thread(new ThreadStart(PrintNumbersOneToHundred));
+        t.Start();
+        PrintNumbersOneToHundred();
+        t.Join();
+        Thread.Sleep(2000);
+        Console.WriteLine("Thread Ended Printing");
     }
-
     public static void Assignment4Part2()
     {
-        /*
-        * Write code here
-        */
+        locker = new object();
+        Thread thread1 = new Thread(new ThreadStart(PrintNumbersOneToHundredWithLock));
+        Thread thread2 = new Thread(new ThreadStart(PrintNumbersOneToHundredWithLock));
+        thread1.Start();
+        thread2.Start();
     }
 }
