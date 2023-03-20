1366a179e3bc66d80b66b8c290ebca479e8128ea
AmirGolomb <gh.amir.251@gmail.com>
2023-03-20 11:29:47 +0200

    7Boom

---
+++ 7Boom/Basic/NumContainer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class NumContainer
+{
+    public int num { get; set; }
+    public NumContainer() 
+    {
+        num = 0;
+    }
+}
+
