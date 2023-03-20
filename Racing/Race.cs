2054e000bfe1f90d8d64d4a4cf3ed07248cd065c
guyalfasi <guyalfasi@gmail.com>
2023-03-20 18:42:27 +0200

    finished car class, tomorrow finish race class and task in general

---
+++ Racing/Race.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Racing
+{
+    internal class Race
+    {
+        public List<Car> cars;
+        public float LengthOfRace;
+
+        public Race(float lengthOfRace)
+        {
+            cars = new List<Car>();
+            LengthOfRace = lengthOfRace;
+        }
+    }
+}
