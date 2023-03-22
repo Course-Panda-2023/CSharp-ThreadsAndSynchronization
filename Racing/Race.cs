2ba67cb8db42a5f34291eea3828f1dc8d723b839
liavbatson <batsonliav@gmail.com>
2023-03-22 10:20:02 +0200

    Racing namespace

--- Racing/Race.cs
+++ Racing/Race.cs
@@ -4,31 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Threads
+public class Race
 {
-    public class Race
+    private int lenghtOfLap;
+    private List<Car> cars;
+    private List<Thread> threads = new List<Thread>();
+    private static int position = 0;
+    public static int Position
     {
-        private int lenghtOfLap;
-        private List<Car> cars;
-        private List<Thread> threads = new List<Thread>();
-        private static int position = 0;
-        public static int Position
+        get { return position; }
+        set { position = value; }
+    }
+    public Race(int lenghtOfLap, List<Car> cars)
+    {
+        this.lenghtOfLap = lenghtOfLap;
+        this.cars = cars;
+    }
+    public void SimulateRace()
+    {
+        foreach (Car car in cars)
         {
-            get { return position; }
-            set { position = value; }
-        }
-        public Race(int lenghtOfLap, List<Car> cars)
-        {
-            this.lenghtOfLap = lenghtOfLap;
-            this.cars = cars;
-        }
-        public void SimulateRace()
-        {
-            foreach (Car car in cars)
-            {
-                threads.Add(new Thread(() => car.SimulateDrive(lenghtOfLap)));
-                threads.Last().Start();
-            }
+            threads.Add(new Thread(() => car.SimulateDrive(lenghtOfLap)));
+            threads.Last().Start();
         }
     }
 }
