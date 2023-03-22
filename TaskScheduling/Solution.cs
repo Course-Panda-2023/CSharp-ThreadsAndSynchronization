6752d09e1410269dbee1f4c58351b3cccf3c2737
liavbatson <batsonliav@gmail.com>
2023-03-22 10:20:27 +0200

    Completed TaskScheduling

--- TaskScheduling/Solution.cs
+++ TaskScheduling/Solution.cs
@@ -1,39 +1,33 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 public class Solution
 {
-    public static void Assignment1()
+    public static string Pow2(int num)
     {
-        /*
-        * Write code here
-        */
+        return $"{num} ^ 2 = {Math.Pow(num, 2)}";
     }
 
-    public static void Assignment2()
+    public static string Pow3(int num)
     {
-        /*
-        * Write code here
-        */
+        return $"{num} ^ 3 ={Math.Pow(num, 3)}";
     }
 
-    public static void Assignment3()
+    public static string Sqrt(int num)
     {
-        /*
-        * Write code here
-        */
+        return $"{num} ^ 0.5 ={Math.Sqrt(num)}";
     }
 
-    public static void Assignment4()
+    public static string Hello(int num)
     {
-        /*
-        * Write code here
-        */
+        return $"Hello {num}";
     }
 
-    public static void Assignment5()
+    public static string Bye(int num)
     {
-        /*
-        * Write code here
-        */
+        return $"Bye {num}";
     }
 }
---
+++ TaskScheduling/TaskScheduling.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Threads
+{
+    public class TaskScheduling <T, V>
+    {
+        private static System.Timers.Timer timer;
+        private static Dictionary<Func<T, V>, int> tasks;
+        private static int currentTime;
+        private T taskInput;
+        public TaskScheduling(T taskInput)
+        {
+            this.taskInput = taskInput;
+            tasks = new Dictionary<Func<T, V>, int>();
+            currentTime = 0;
+            StartTimer();
+        }
+        public void StartTimer()
+        {
+            timer = new System.Timers.Timer();
+            timer.Elapsed += new ElapsedEventHandler(TimeListener);
+            timer.Interval = 1000;
+            timer.Start();
+        }
+        public void TimeListener(object sender, ElapsedEventArgs e)
+        {
+            currentTime++;
+            Console.WriteLine($"Current Time Is: {currentTime}");
+            CheckForTask();
+        }
+        public void AddTask(Func<T, V> task, int time)
+        {
+            tasks.Add(task, time);
+            Console.WriteLine($"Task Added To Time: {time}");
+        }
+        public void RemoveTask(Func<T, V> task)
+        {
+            if (tasks.ContainsKey(task))
+            {
+                int taskTime = tasks[task];
+                if (taskTime <= currentTime)
+                    Console.WriteLine("Too Late, Task Already Done !");
+                else
+                {
+                    tasks.Remove(task);
+                    Console.WriteLine("Task Removed !");
+                }
+            }
+        }
+        public void CheckForTask()
+        {
+            if (tasks.Count == 0)
+            {
+                Console.WriteLine("All Tasks Done !");
+                timer.Stop();
+            }
+            else
+                foreach (var task in tasks)
+                {
+                    if (task.Value == currentTime)
+                    {
+                        Func<T, V> taskFunc = task.Key;
+                        Console.WriteLine(taskFunc(taskInput));
+                        tasks.Remove(taskFunc);
+                    }
+                }
+        }
+    }
+}
