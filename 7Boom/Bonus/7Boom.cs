cd96e145a44dc6619daf7daf647eadd1e560fe46
‪Ilan Motiei‬‏ <ilanmotiei@mail.tau.ac.il>
2023-03-29 16:36:24 +0300

    submitted

--- 7Boom/Bonus/7Boom.cs
+++ 7Boom/Bonus/7Boom.cs
@@ -1,9 +1,48 @@
 ﻿using System;
+using System.Threading;
 
-namespace Bonus
+public class SevenBoomBonus
 {
-    public class SevenBoomBonus
-    {
+    private int _maxNumber;
+    private int _counter = 1;
+    private object _lock = new object();
 
+    public SevenBoomBonus(int maxNumber)
+    {
+        _maxNumber = maxNumber;
+    }
+
+    public void Run(int numThreads, Action mCallBack)
+    {
+        CountdownEvent countdown = new CountdownEvent(numThreads);
+        
+        for (int i = 1; i <= numThreads; i++)
+        {
+            ThreadPool.QueueUserWorkItem(new WaitCallback((object state) =>
+            {
+                string message;
+                
+                while (true)
+                {
+                    Thread.Sleep(100);
+                    lock (_lock)
+                    {
+                        if (_counter == _maxNumber + 1)
+                        {
+                            break;
+                        }
+                        message = (_counter % 7 == 0) || _counter.ToString().Contains("7") ? "Boom" : _counter.ToString();
+                        Console.WriteLine(message);
+                        
+                        mCallBack();
+                        _counter++;
+                    }
+                }
+                
+                countdown.Signal();
+            }));
+        }
+
+        countdown.Wait();
     }
 }
--- 7Boom/Program.cs
+++ 7Boom/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Basic;
+
+
+
+// SevenBoom game = new SevenBoom(maxNumber: 200);
+// game.Run(4);
+
+// SevenBoomPool game2 = new SevenBoomPool(maxNumber: 200);
+// game2.Run(numThreads: 4, s =>
+// {
+//     Console.WriteLine(s);
+// });
+
+SevenBoomBonus game3 = new SevenBoomBonus(maxNumber: 200);
+game3.Run(numThreads:4, () =>
+{
+    Console.WriteLine("inside the callback");
+});
+
+
